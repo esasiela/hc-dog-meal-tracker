cfb09e4fb7917a15b9efa7a3351793a069eded1f
esasiela <esasiela@gmail.com>
2018-11-17 12:26:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hc-dog-meal-tracker
---
+++ hc-dog-meal-tracker.ino
@@ -0,0 +1,87 @@
+
+#include <HC_BouncyButton.h>
+
+#define PIN_LED1 0
+#define PIN_LED2 1
+#define PIN_BUTTON1 2
+
+// how long to live in state 1 (breakfast served) in millis (54000000 = 15 hours)
+#define TTL_BREAKFAST 54000000
+
+// how long to live in state 2 or 3 (dinner served, independent of whether breakfast was served) in millis (28,800,000 = 8 hours)
+#define TTL_DINNER 28800000
+
+BouncyButton btn=BouncyButton(PIN_BUTTON1);
+
+#define MAX_MEAL_STATE 4
+
+/* 
+ * 0=reset, no meals
+ * 1=breakfast, no dinner 
+ * 2=dinner, no breakfast
+ * 3=breakfast and dinner (happy pups)
+ */
+byte mealState;
+
+static unsigned long ttlMillis;
+
+void setup() {
+  pinMode(PIN_LED1, OUTPUT);
+  digitalWrite(PIN_LED1, LOW);
+  
+  pinMode(PIN_LED2, OUTPUT);
+  digitalWrite(PIN_LED2, LOW);
+
+  pinMode(PIN_BUTTON1, INPUT_PULLUP);
+
+  btn.init();
+}
+
+void loop() {
+
+  // trigger on press (not release)
+  if (btn.update() && !btn.getState()) {
+    mealState++;
+    if (mealState>=MAX_MEAL_STATE) {
+      mealState=0;
+
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, LOW);
+        
+    } else if (mealState==1) {
+      digitalWrite(PIN_LED1, HIGH);
+      digitalWrite(PIN_LED2, LOW);
+      ttlMillis = millis() + TTL_BREAKFAST;
+        
+    } else if (mealState==2) {
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, HIGH);
+      ttlMillis = millis() + TTL_DINNER;
+      
+    } else if (mealState==3) {
+      digitalWrite(PIN_LED1, HIGH);
+      digitalWrite(PIN_LED2, HIGH);
+      ttlMillis = millis() + TTL_DINNER;
+      
+    } else {
+      // should never reach this, loop forever in hell
+      digitalWrite(PIN_LED1, LOW);
+      digitalWrite(PIN_LED2, HIGH);
+
+      while (true) {
+        digitalWrite(PIN_LED1, !digitalRead(PIN_LED1));
+        digitalWrite(PIN_LED2, !digitalRead(PIN_LED2));
+        delay(1000);
+      }
+    }
+  }
+
+  // did the timer reach the reset duration?
+  if (mealState>0 && (long)(millis()-ttlMillis)>=0) {
+    // we've been in a non-reset state too long, reset the state
+    mealState=0;
+    digitalWrite(PIN_LED1, LOW);
+    digitalWrite(PIN_LED2, LOW);
+  }
+}
+
